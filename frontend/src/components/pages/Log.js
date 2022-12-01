dad887366dc43332b88ed2102a7f5bc1951c271b
Emile Le Corre <emile.21@hotmail.fr>
2022-12-01 15:32:56 +0100

    refonte des form login & signup

--- frontend/src/components/pages/Log.js
+++ frontend/src/components/pages/Log.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Log from "../Log";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/MASTERmanga.png";
 
 const Profil = () => {
   return (
     <div className="profil-page">
+      
       <img
         src={logo}
         alt="logo Groupomania"
         className="profil-page__logo-site"
       />
+
       <div className="profil-page__log-container">
         {/* On donne des proprieter "props" aux formulaire afin de s'avoir le quel des deux formulaire s'affichera en premier */}
         <Log signin={false} signup={true} />
